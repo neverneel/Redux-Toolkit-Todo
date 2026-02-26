fec6304f727410de7bb9a511750c5c62b93fcb0c
NEEL <neelkamalpunj@gmail.com>
2026-02-27 00:18:23 +0530

    edited

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {useDispatch} from 'react-rerdux'
+import {useDispatch} from 'react-redux'
 import {addTodo} from '../features/todo/todoSlice'
 
 function AddTodo() {
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
+import { removeTodo } from '../features/todo/todoSlice'
 
 function Todos() {
+  const todos = useSelector(state => state.todos)
+  const dispatch = useDispatch()
   return (
-    <div>
-      
-    </div>
+    <>
+     <div>Todos</div> 
+     {todos.map((todo) => (
+      <li key={todo.id}>
+        {todo.text}
+        <button onClick={() => dispatch(removeTodo(todo.id))}>X</button>
+      </li>
+     ))}
+    </>
   )
 }
 
